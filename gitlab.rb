cbd438378baeced7dac44c48687e0305998a0469
manojkumar.t <manojkumar.t@shloklabs.com>
2023-04-06 13:58:28 +0530

    git changes

--- gitlab.rb
+++ gitlab.rb
@@ -1,2 +1,2 @@
-external_url 'http://localhost/'
+external_url 'http://team.pro-inspector.net/'
 gitlab_rails['initial_root_password'] = File.read('/run/secrets/gitlab_root_password').gsub("\n", "")
